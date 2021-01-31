3cc41cbe3976164e23b48e19b5db9eca440a16b2
Pythonidaer <pythonidaer@gmail.com>
2021-01-31 18:11:29 -0500

    Worked with my tutor to re-cover localStorage. I added Luxon for the dates, added some more functions to make each added city button clickable. Also updated the README.md

--- README.md
+++ README.md
@@ -1,54 +1,76 @@
 # WeatherDashboard
+*General Weather search utilizing OpenWeatherAPI calls, localStorage and jQuery*
 
+[View Deployed Application Here.](https://pythonidaer.github.io/WeatherDashboard/)
 
+## Installation
+1. Clone repository to your local device
+2. Test by opening the index.html file in your browser. If it looks good, it's working right.
+3. If it does not work, right-click Inspect console to determine if there is an issue with the directories.
+4. If code runs but Inspector logs error, you may need to go to OpenWeatherAPI to sign up for a new API key.
 
+## Usage
+This repo is simply a homework assignment. Feel free to inspect the code and notice how I made a Weatherboard API
+- Incorporating Luxon.js and using the math feature to add days and get time even considering new months
+- Using jQuery to select elements more easily, and to add attributes with less code
+- Nesting `$.ajax` calls to chain queries reliant on the parent Promise resolving correctly
+- Bootstrap utilization for simple search/delete/buttons code including simple layout
+- How to dynamically add a class to an element based on integer conditions sourced from an API call
 
+## Credits
+[UV Index Scale](https://www.epa.gov/sunsafety/uv-index-scale-0);
 
+[World Clock](https://www.timeanddate.com/worldclock/);
 
+[jQuery .addClass()](https://api.jquery.com/addclass/);
 
+[MDN parseFloat()](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseFloat);
 
+[Degree HTML Code](https://www.toptal.com/designers/htmlarrows/math/degree-sign/);
 
+[Current weather data](https://openweathermap.org/current#parameter);
 
+[jQuery val()](https://www.w3schools.com/jquery/html_val.asp);
 
+[Clear inputs after submission](https://stackoverflow.com/questions/14589193/clearing-my-form-inputs-after-submission);
 
+[Create elements in jQuery](https://stackoverflow.com/questions/867916/creating-a-div-element-in-jquery);
 
+[Weather Conditions Codes](https://openweathermap.org/weather-conditions#Weather-Condition-Codes-2);
 
+[5-day Weather Forecast](https://openweathermap.org/forecast5);
 
+[Current Weather](https://openweathermap.org/current#parameter);
 
+[One Call API](https://openweathermap.org/api/one-call-api);
 
+[jQuery attr()](https://www.tutorialrepublic.com/faq/how-to-change-the-image-source-using-jquery.php#:~:text=Answer%3A%20Use%20the%20jQuery%20attr,img%3E%20tag)%20in%20jQuery.);
 
-
-
-
-
+[JSON:API module](https://www.drupal.org/docs/core-modules-and-themes/core-modules/jsonapi-module);
 
 [UV Index Scale](https://www.epa.gov/sunsafety/uv-index-scale-0);
-[World Clock](https://www.timeanddate.com/worldclock/);
-[jQuery .addClass()](https://api.jquery.com/addclass/);
-[MDN parseFloat()](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseFloat);
-[Degree HTML Code](https://www.toptal.com/designers/htmlarrows/math/degree-sign/);
-[Current weather data](https://openweathermap.org/current#parameter);
-[]();
-[]();
-[]();
-[]();
-[]();
-[]();
-[]();
-[]();
-[]();
-[]();
-[]();
-[]();
-[]();
-[]();
-[]();
-[]();
-[]();
-[]();
-[]();
-[]();
-[]();
-[]();
-[]();
-[]();
+
+[How to ask a good Stack Overflow question](https://stackoverflow.com/help/how-to-ask);
+
+[Font Awesome Icons](https://fontawesome.com/v4.7.0/icon/trash-o);
+
+[Luxon Calendar Math](https://moment.github.io/luxon/docs/manual/math.html);
+
+## Future Improvements
+1. Make page load to top-most (most recent) city search
+2. Change date syntax to represent MM/DD/YYYY
+3. Specify location as to assure user that when they search 'Salem' it is 'MA' not 'OR' and whatnot
+4. Test out other Weather APIs
+5. Refactor code so that multiple of the same $.ajax() calls aren't made (it's extremely not-DRY)
+6. Learn how to write AJAX calls within functions that can be called later (I tried but kept getting error)
+7. Improve mobile responsiveness
+8. Add Modal for when city cannot generate Weather
+9. Research catch/try/error/resolve methods to utilize code for when Promises resolve negatively (then #8)
+10. Recreate in Bulma, because Bootstrap is both amazing and somewhat an eye sore when it's out-of-the-box
+  
+## License
+I've chosen an MIT License. Do what you'd like with this material.
+
+#### The Website
+![Weatherboard API](assets/images/screenshot.png)
+![Lighthouse Rankings](assets/images/lighthouse.png)
--- assets/scripts/behaviour.js
+++ assets/scripts/behaviour.js
@@ -1,27 +1,43 @@
 $( document ).ready(function() {
-  let searchInput = $('#search-input');
-  let searchBtn = $('#search-btn')
+  // Add submitted search queries into an array of city names
   let searchArr = [];
-  let searchList = $('#search-list');
-  let cityname = "denver";
+  // Declare variable to eventually be used as query parameter
+  let cityname;
+  // WeatherAPI key, if this does not work after 1/31/2021 Just create one with OpenWeatherAPI and replace.
   let APIKEY = 'c18d5d2e20b21d5580498fa1824aba22';
-  // api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-  let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityname + `&appid=${APIKEY}&units=imperial`;
-  // api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
-  // http://api.openweathermap.org/data/2.5/forecast?q=denver&appid=c18d5d2e20b21d5580498fa1824aba22
-  let weeklyForecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityname + `&appid=${APIKEY}&units=imperial`;
-  // alias luxon object for improved readability
+  // Luxon Library to get local time and alias;
   let DateTime = luxon.DateTime;
   let dt = DateTime.local();
-  // Top row dedicated solely to today's weather
-  let todaysDate = `${dt.month}/${dt.day}/${dt.year}`
-  // 5-Day Forecast Dates:
-  let forecastD1 = `${dt.month}/${dt.day + 1}/${dt.year}`
-  let forecastD2 = `${dt.month}/${dt.day + 2}/${dt.year}`
-  let forecastD3 = `${dt.month}/${dt.day + 3}/${dt.year}`
-  let forecastD4 = `${dt.month}/${dt.day + 4}/${dt.year}`
-  let forecastD5 = `${dt.month}/${dt.day + 5}/${dt.year}`
-// Function completely covers dates; will need updating based on localStorage items.
+  // toISODate() appeasr to be YYYY-MM-DD
+  let todaysDate = dt.toISODate();
+  // 5-Day Forecast
+  let forecastD1 = dt.plus({ days: 1 }).toISODate()
+  let forecastD2 = dt.plus({ days: 2 }).toISODate()
+  let forecastD3 = dt.plus({ days: 3 }).toISODate()
+  let forecastD4 = dt.plus({ days: 4 }).toISODate()
+  let forecastD5 = dt.plus({ days: 5 }).toISODate()
+
+  // Get storage object by key name and parse it
+  function getCityBtn() {
+    searchArr = JSON.parse(localStorage.getItem("cities"));
+    // If it exists, create a button for each array item and log it to the #search-list div
+    if (searchArr) {
+      for (let i = 0; i < searchArr.length; i++) {
+        let citySearch = $(`<button>${searchArr[i]}</button>`).addClass('btn btn-outline-secondary bg-white w-100 city-btn').attr('type', 'button');
+        $('#search-list').append(citySearch);
+      }
+      // If there is no storage object, make sure searchArr is still just an empty array.
+    } else {
+      searchArr = [];
+    }
+    // This helps me figure out what the first cityname is on the list to try and pre-populate
+    // cityname = searchArr[0];
+    // console.log(cityname);
+    
+  }
+  getCityBtn();
+
+// Top date in white card, 5-Day Forecast in bottom blue tiles
   function headingDates() {
     // today's weather
     $('#current-date').text(todaysDate)
@@ -34,104 +50,65 @@ $( document ).ready(function() {
   }
   headingDates();
 
+
   // Make submit button take value, add it to an array, clear screen, and make a button, then add to storage
-  searchBtn.on('click', function(event) {
+  $('#search-btn').on('click', function(event) {
     event.preventDefault();
+    // User's input search is the query parameter cityname
+    let savedSearch = $('#search-input').val();
+    cityname = savedSearch;
+    // City weather is queryURL, and 5-Day forecast is the weeklyForecast;
+    let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityname + `&appid=${APIKEY}&units=imperial`;
+    let weeklyForecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityname + `&appid=${APIKEY}&units=imperial`;
 
-    // Does not work
-    // getCityBtn();
-    // function getCityBtn() {
-    //   var searchArrParsed = JSON.parse(localStorage.getItem("cities"));
-    //   for (let i = 0; i < searchArrParsed.length; i++) {
-    //     let citySearch = $(`<button>${searchArrParsed[0]}</button>`).addClass('btn btn-outline-secondary bg-white w-100');
-    //     searchList.prepend(citySearch);
-    //     searchArrParsed = [];
-    //   }
-    // }
-
-    let savedSearch = searchInput.val();
-
-    function storeCityBtn() {
-      localStorage.setItem("cities", JSON.stringify(searchArr));
-    }
-    
-    function createCityBtn() {
-      searchArr.unshift(savedSearch);
-      searchInput.val('');
-      // console.log(searchArr);
-      let citySearch = $(`<button>${searchArr[0]}</button>`).addClass('btn btn-outline-secondary bg-white w-100');
-      searchList.prepend(citySearch);
-      storeCityBtn();
-    }
-
-    if (savedSearch.length !== 0) {
-      createCityBtn();
-    }
-  })
-
-
-  // This call writes the cityname, current-weather(temp, humidity), & sends a 2nd AJAX Call for UVIndex all for the top card 
+    // Call ajax when search icon is clicked
     $.ajax({
       url: queryURL,
       method: "GET"
     }).then(function(response) {
-      // console.log(response);
+      // Popular top card weather data
       $('#city').text(response.name);
       function todaysWeather() {
           $('#current-temp').text(response.main.temp);
           $('#current-humidity').text(response.main.humidity);
           $('#current-wind-speed').text(response.wind.speed);
-    }
-    todaysWeather();
-          let lat = response.coord.lat;
-          let lon = response.coord.lon;
-            // Current and forecast weather data 
-            //  https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
-          let oneCallAPI = "https://api.openweathermap.org/data/2.5/onecall?" + `lat=${lat}&` + `lon=${lon}&` + `appid=${APIKEY}&units=imperial`;
-              $.ajax({
-                url: oneCallAPI,
-                method: "GET"
-              }).then(function(uvIndex) {
-                $('#current-uvi').text(uvIndex.current.uvi);
-                // change UVI background by scale
-                function uvIndexScale() {
-                  let uvScale = $('.uv-scale');
-                  let currentUV = parseFloat($('#current-uvi').text()) // float
-                  if (currentUV < 3) {
-                    uvScale.addClass("uv-low");
-                  } else if (currentUV >= 3 && currentUV < 6) {
-                    uvScale.addClass("uv-moderate");
-                  } else if (currentUV >= 6 && currentUV < 8) {
-                    uvScale.addClass("uv-high");
-                  } else if (currentUV >= 8 && currentUV < 11) {
-                    uvScale.addClass("uv-very-high");
-                  } else if (currentUV >= 11) {
-                    uvScale.addClass("uv-extreme");
-                  };
-                }; 
-                uvIndexScale();
-    // $('.btn-city').on('click', function() {
-    //     console.log($(this));
-    // });
-  });
+      }
+      todaysWeather();
+      // Store key values lattitude and longitude because we are going to use that to search for UV data
+      let lat = response.coord.lat;
+      let lon = response.coord.lon;
+      // Query the one call API to obtain UV Index data
+      let uvURL = "https://api.openweathermap.org/data/2.5/onecall?" + `lat=${lat}&` + `lon=${lon}&` + `appid=${APIKEY}&units=imperial`;
+      $.ajax({
+        url: uvURL,
+        method: "GET"
+      }).then(function(uvIndex) {
+        $('#current-uvi').text(uvIndex.current.uvi);
+        // change UVI background by scale, making background green, yellow, etc.
+        function uvIndexScale() {
+          let uvScale = $('.uv-scale');
+          let currentUV = parseFloat($('#current-uvi').text()) // float
+          if (currentUV < 3) {
+            uvScale.addClass("uv-low");
+          } else if (currentUV >= 3 && currentUV < 6) {
+            uvScale.addClass("uv-moderate");
+          } else if (currentUV >= 6 && currentUV < 8) {
+            uvScale.addClass("uv-high");
+          } else if (currentUV >= 8 && currentUV < 11) {
+            uvScale.addClass("uv-very-high");
+          } else if (currentUV >= 11) {
+            uvScale.addClass("uv-extreme");
+          };
+        }; 
+        uvIndexScale();
+      });
     });
-    
-  // This SECOND Call grabs the weather-icon based on weather description, 5-day forecast temp, and humidityh, 
+    // Use same cityname to call the 5-day Weather Forecast
     $.ajax({
       url: weeklyForecast,
       method: "GET"
     }).then(function(response) {
-      // weatherDescArr is a collection of the returned object's 5-day 12pm forecast weather main "descriptions"
-      // WEATHER CONDITION CODES: https://openweathermap.org/weather-conditions#Weather-Condition-Codes-2
-      /* Each "description" 
-        Main: clouds - 03d
-        Main: clear - 01d
-        Main: snow - 13d
-        Main: Rain - 10d
-        Main: Drizzle - 09d
-        Main: Thunderstorm - 11d
-        Main: Mist/Smoke/Haze/Dust/Fog/Sand/Dust/Ash/Squall/Tornado - 50d
-      */
+      // Depending on the weather's main description word, populate the corresponding image
       function setWeatherIcons() {
         let dayIconsURL = "http://openweathermap.org/img/wn/";
         let weatherDescArr = [
@@ -183,26 +160,143 @@ $( document ).ready(function() {
       }
       fiveDayForecast();
     });
+
+    // Each city search that has been added to the array, stringify that array and set to local storage
+    function storeCityBtn() {
+      localStorage.setItem("cities", JSON.stringify(searchArr));
+    }
+    
+    // Add searched city into the array (from beginning), clear input value, add button to search-list
+    function createCityBtn() {
+      searchArr.unshift(savedSearch);
+      $('#search-input').val('');
+      let citySearch = $(`<button>${searchArr[0]}</button>`).addClass('btn btn-outline-secondary bg-white w-100 city-btn').attr('type', 'button');
+      $('#search-list').prepend(citySearch);
+      storeCityBtn();
+      hearCityBtn();
+    }
+
+    if (savedSearch.length !== 0) {
+      createCityBtn();
+    }
+  })
+
+  // For each city button in the #search-list, if it's clicked then get the weather
+  function hearCityBtn() {
+    $('.city-btn').each(function() {
+      $(this).on('click', function() {
+        console.log(this.innerText);
+
+        let savedSearch = this.innerText;
+    
+        cityname = savedSearch;
+        let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityname + `&appid=${APIKEY}&units=imperial`;
+        let weeklyForecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityname + `&appid=${APIKEY}&units=imperial`;
+
+        $.ajax({
+          url: queryURL,
+          method: "GET"
+        }).then(function(response) {
+          $('#city').text(response.name);
+          function todaysWeather() {
+              $('#current-temp').text(response.main.temp);
+              $('#current-humidity').text(response.main.humidity);
+              $('#current-wind-speed').text(response.wind.speed);
+        }
+        todaysWeather();
+              let lat = response.coord.lat;
+              let lon = response.coord.lon;
+              let uvURL = "https://api.openweathermap.org/data/2.5/onecall?" + `lat=${lat}&` + `lon=${lon}&` + `appid=${APIKEY}&units=imperial`;
+                  $.ajax({
+                    url: uvURL,
+                    method: "GET"
+                  }).then(function(uvIndex) {
+                    $('#current-uvi').text(uvIndex.current.uvi);
+                    function uvIndexScale() {
+                      let uvScale = $('.uv-scale');
+                      let currentUV = parseFloat($('#current-uvi').text()) // float
+                      if (currentUV < 3) {
+                        uvScale.addClass("uv-low");
+                      } else if (currentUV >= 3 && currentUV < 6) {
+                        uvScale.addClass("uv-moderate");
+                      } else if (currentUV >= 6 && currentUV < 8) {
+                        uvScale.addClass("uv-high");
+                      } else if (currentUV >= 8 && currentUV < 11) {
+                        uvScale.addClass("uv-very-high");
+                      } else if (currentUV >= 11) {
+                        uvScale.addClass("uv-extreme");
+                      };
+                    }; 
+                    uvIndexScale();
+          });
+        });
+        
+        $.ajax({
+          url: weeklyForecast,
+          method: "GET"
+        }).then(function(response) {
+          function setWeatherIcons() {
+            let dayIconsURL = "http://openweathermap.org/img/wn/";
+            let weatherDescArr = [
+            response.list[3].weather[0].main,
+            response.list[11].weather[0].main,
+            response.list[19].weather[0].main,
+            response.list[27].weather[0].main,
+            response.list[35].weather[0].main
+            ];
+            let day = 1;
+            for (let i = 0; i < weatherDescArr.length; i++) {
+              if (weatherDescArr[i] === "Clouds") {
+                $(`#img-d${day}`).attr('src', dayIconsURL + '03d@2x.png');
+              } else if (weatherDescArr[i] === "Clear") {
+                $(`#img-d${day}`).attr('src', dayIconsURL + '01d@2x.png');
+              } else if (weatherDescArr[i] === "Snow") {
+                $(`#img-d${day}`).attr('src', dayIconsURL + '13d@2x.png');
+              } else if (weatherDescArr[i] === "Rain") {
+                $(`#img-d${day}`).attr('src', dayIconsURL + '10d@2x.png');
+              } else if (weatherDescArr[i] === "Drizzle") {
+                $(`#img-d${day}`).attr('src', dayIconsURL + '09d@2x.png');
+              } else if (weatherDescArr[i] === "Thunderstorm") {
+                $(`#img-d${day}`).attr('src', dayIconsURL + '11d@2x.png');
+              } else {
+                $(`#img-d${day}`).attr('src', dayIconsURL + '50d@2x.png');
+              }
+              day++;
+            }
+          }
+          setWeatherIcons();
+    
+          // 5 day forecast, Temp: & Humidity:
+          function fiveDayForecast() {
+            // Current day + 1 12:00:00
+            $('#temp-d1').text(response.list[3].main.temp);
+            $('#humidity-d1').text(response.list[3].main.humidity);
+            // Current day + 2 12:00:00
+            $('#temp-d2').text(response.list[11].main.temp);
+            $('#humidity-d2').text(response.list[11].main.humidity);
+            // Current day + 3 12:00:00
+            $('#temp-d3').text(response.list[19].main.temp);
+            $('#humidity-d3').text(response.list[19].main.humidity);
+            // Current day + 4 12:00:00
+            $('#temp-d4').text(response.list[27].main.temp);
+            $('#humidity-d4').text(response.list[27].main.humidity);
+            // Current day + 5 12:00:00
+            $('#temp-d5').text(response.list[35].main.temp);
+            $('#humidity-d5').text(response.list[35].main.humidity);
+          }
+          fiveDayForecast();
+        });
+
+      })
+    })
+  }
+  hearCityBtn();
+
+  // When red trash icon is clicked, clear local storage, empty the search array, and erase city buttons
+  $('#clear-btn').on('click', function(event) {
+      event.preventDefault();
+    $('#search-list').empty();
+    localStorage.clear();
+    searchArr = [];
+  })
 });
-
-
-
-// WHEN U OPEN WEATHER DASHBOARD IT POPULATES THE LAST SEARCHED CITY
-// I want to type a query that receives input from the input search and queries based on that q="city"
-// every time the search button is clicked, it acts as a to-do list and adds the city to the buttons field
-// every time the button field is updated, it should set these to local storage, probably from an array
-// each time the page is refreshed, it should display this list of buttons
-// each button, when clicked, should call an ajax request for the weather for a query using its var q="city"
-// it is not required, but a delete icon would be nice
-// I want to be able to log individual pieces of data such as response.temperature
-// Then I want to create an HTML empty template where the text content is populated from the API data values
-// I also want the screen to be mostly blank save for the input field div for search
-// on click of each city, the template repopulates so maybe a call function
-
-
-/*
-
-*/
-
-
-
